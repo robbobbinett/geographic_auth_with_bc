0df48ad59a5af42783815cdf3539ed14001ed10a
robbobbinett <robinettryan8@gmail.com>
2019-10-30 14:09:30 -0500

    basic universe functionality implemented

--- simulation/node_props.py
+++ simulation/node_props.py
@@ -1,5 +1,6 @@
 from random import choice, sample
 from scipy.stats import bernoulli
+from scipy.sparse import csc_matrix
 import numpy as np
 
 def default_drop(self):
@@ -82,6 +83,12 @@ class universe_wrapper:
 			raise TypeError("All items in universe should be of type person_node. Currently, universe contains instances of the following types: "+", ".join(set([str(type(item)) for item in universe])))
 		self.universe = universe
 
+		self.node_names = [str(node) for node in self.universe]
+		self.node_names.sort()
+
+		self.name_to_node = dict((str(node), node) for node in self.universe)
+		self.node_to_number = dict((item, j) for j, item in enumerate(self.node_names))
+
 		if not isinstance(percentage_update_action, float):
 			raise TypeError("percentage_update_action should be of type float; currently of type "+str(type(percentage_update_action))+".")
 		self.percentage_update = percentage_update_action
@@ -90,6 +97,13 @@ class universe_wrapper:
 		to_update = sample(self.universe, np.floor(self.percentage_update*len(self.universe)))
 		for node in to_update:
 			node.update_action()
+
+	def output_connections(self):
+		connections_matrix = np.zeros((len(self.universe),)*2)
+		for j, name in enumerate(self.node_names):
+			for neigh in self.name_to_node[name].neighbors:
+				connections_matrix[j, self.name_to_number[neigh]] = 1
+		return csc_matrix(connections_matrix)
 
 def make_universe_of_nodes(num_nodes, add_behavior=default_add, drop_behavior=default_drop, pass_prob=0.5, get_add_prob=default_get_add_prob):
 	universe = set()
