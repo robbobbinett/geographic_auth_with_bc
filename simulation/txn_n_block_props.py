2ff32cab3215350d12c08b7604194d27ba150a7c
robbobbinett <robinettryan8@gmail.com>
2019-11-02 13:03:43 -0500

    documentation for block primitives

--- simulation/txn_n_block_props.py
+++ simulation/txn_n_block_props.py
@@ -1,4 +1,8 @@
 class free_block:
+	"""
+	The primitive for a block (i.e. a nonce-satisfied problem that is
+	acceptable for committing to a local blockchain
+	"""
 	def __init__(self, unique_id, parent=None):
 		if not isinstance(unique_id, int):
 			raise TypeError("parent must be of type free_block; currently of type "+str(type(parent))+".")
@@ -21,9 +25,15 @@ class free_block:
 		return str(self)
 
 	def __hash__(self):
+		"""
+		Hashing by self.id allows for inclusion in sets, easy dict mapping, etc.
+		"""
 		return hash(self.id)
 
 	def __eq__(self, other):
+		"""
+		For equality to hold, both names and parents must be identical
+		"""
 		if not isinstance(other, free_block):
 			raise TypeError("other must be of type free_block; currently of type "+str(type(other))+".")
 		return self.id == other.id and self.parent == other.parent
@@ -31,6 +41,10 @@ class free_block:
 null_block = free_block(0)
 
 class fixed_block:
+	"""
+	Primitive for taking a valid block (to be committed to a local blockchain)
+	and installing it into a local chain. Creates a rooted tree structure
+	"""
 	def __init__(self, free_seed, parent=None):
 		if not isinstance(free_seed, free_block):
 			raise TypeError("free_seed must be of type free_block; currently of type "+str(type(free_seed))+".")
@@ -44,20 +58,36 @@ class fixed_block:
 				raise TypeError("parent must be of type fixed_block; currently of type "+str(type(parent))+".")
 		self.parent = parent
 
+		# self.height is the number of inheritances (parent-child
+		# relationships) from root to self.
 		if self.parent is None:
 			self.height = 0
 		else:
 			self.height = parent.height + 1
 
+		# list of child nodes, which is updated and sorted by
+		# self.block.id
 		self.children = []
+
+		# self.shoulder_weight is the number of nodes which, by
+		# transitivity of childhood, inherit from self. computed
+		# using self.cascading_shoulder_weight_add
 		self.shoulder_weight = 0
 
 	def cascading_shoulder_weight_add(self):
+		"""
+		Every time a child node is appended, increase by 1 the shoulder
+		weight of all nodes from whom the new node receives inheritance
+		"""
 		self.shoulder_weight += 1
 		if self.parent is not None:
 			self.parent.cascading_shoulder_weight_add()
 
 	def add_child(self, free_child):
+		"""
+		Insert a child node into tree, and update the parameters of all
+		parent/child nodes accordingly
+		"""
 		if not isinstance(free_child, free_block):
 			raise TypeError("child must be of type free_block; currently of type "+str(type(child))+".")
 		fixed_child = fixed_block(free_child, self)
@@ -67,12 +97,18 @@ class fixed_block:
 		return fixed_child
 
 	def get_root(self):
+		"""
+		Get root of the current tree.
+		"""
 		if self.parent is None:
 			return self
 		else:
 			return self.parent.get_root()
 
 	def __eq__(self, other):
+		"""
+		self == other iff self.block == other.block
+		"""
 		if not isinstance(other, fixed_block):
 			raise TypeError("other should be of type fixed_block; currently of type "+str(type(other))+".")
 		return self.block == other.block
