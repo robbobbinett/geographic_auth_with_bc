ed5e774e7ccc9d9cf6b32049a7d7cb20e0cbae60
robbobbinett <robinettryan8@gmail.com>
2019-11-10 15:28:52 -0600

    changes on linux.cs.uchicago.edu

--- simulation/message_props.py
+++ simulation/message_props.py
@@ -49,6 +49,7 @@ class cooperative_node(person_node):
 		self.open_problems = set()
 		self.closed_problems = {null_block: fixed_block(null_block)}
 		self.problem_posed = False
+		self.message_queue =[]
 
 	def find_prob_message_by_author(self, orig_author):
 		for x in self.open_problems:
@@ -69,6 +70,36 @@ class cooperative_node(person_node):
 		blocks = list(self.closed_problems.values())
 		blocks.sort(key=lambda x: x.height)
 		return blocks[:num_highest_nodes_to_return]
+
+	def process_queued_message(self):
+		if len(self.message_queue) != 0:
+			message_instance = self.message_queue.pop(0)
+			if not isinstance(message_instance, message):
+				raise TypeError("message_instance should be of type message; currently of type "+str(type(message_instance))+".")
+
+			if message_instance.message_type == "proposal":
+				if message_instance.orig_author == self:
+					pass
+				elif message_instance.orig_author in [x.orig_author for x in self.open_problems]:
+					pass
+				else:
+					self.open_problems.add(message_instance)
+					for neigh in self.neighbors:
+						if neigh != self:
+							neigh.message_queue.append(message_instance)
+
+			if message_instance.message_type == "solution":
+				temp_list = list(self.closed_problems.keys())
+				if message_instance.block not in temp_list:
+					if message_instance.block.parent in temp_list:
+						if message_instance.orig_author in [x.orig_author for x in self.open_problems]:
+							self.open_problems.remove(self.find_prob_message_by_author(message_instance.orig_author))
+						self.add_fixed_block(message_instance)
+						if message_instance.orig_author == self:
+							self.problem_proposed = False
+						for neigh in self.neighbors:
+							if neigh != self:
+								neigh.message_queue.append(message_instance)
 
 	def pass_message(self, message_instance, other):
 		if not isinstance(message_instance, message):
@@ -132,7 +163,8 @@ class cooperative_wrapper(universe_wrapper):
 			count += 1
 		winner.add_fixed_block(solved_problem)
 		for neigh in winner.neighbors:
-			winner.pass_message(message(solved_problem.block, "solution", solved_problem.orig_author, winner), neigh)
+			neigh.message_queue.append(message(solved_problem.block, "solution", solved_problem.orig_author, winner))
+#			winner.pass_message(message(solved_problem.block, "solution", solved_problem.orig_author, winner), neigh)
 
 	def pose_problems(self):
 		for node in self.universe:
@@ -141,7 +173,8 @@ class cooperative_wrapper(universe_wrapper):
 				new_free_block = free_block(self.get_next_block_id(), node.get_highest_blocks()[0].block)
 				prob_message = message(new_free_block, "proposal", node)
 				for neigh in node.neighbors:
-					node.pass_message(prob_message, neigh)
+					neigh.message_queue.append(prob_message)
+#					node.pass_message(prob_message, neigh)
 
 def make_cooperative_wrapper(num_nodes, add_behavior=default_add, drop_behavior=default_drop, pass_prob=0.5, get_add_prob=default_get_add_prob):
 	universe = set()
