c6776da2d3079bb068f03227fa02edb262428ab9
Tiago Royer <royertiago@gmail.com>
2019-12-03 21:24:30 -0600

    test_rigged_rng.py: Test extending the lists.

--- simulation/tests/test_rigged_rng.py
+++ simulation/tests/test_rigged_rng.py
@@ -1,6 +1,7 @@
 from rigged_rng import make_rigged_rng
 
 def test_rigged_rng():
+	# Test basic functionality
 	rigged_rng = make_rigged_rng([0, 1, 0, 3])
 	assert rigged_rng() == 0
 	assert rigged_rng() == 1
@@ -14,6 +15,7 @@ def test_rigged_rng():
 	except:
 		assert(False)
 
+	# Test interleaving rngs
 	rigged_rng = make_rigged_rng([10, 11, 12])
 	other_rng = make_rigged_rng([20, 21, 22])
 	assert rigged_rng() == 10
@@ -22,5 +24,12 @@ def test_rigged_rng():
 	assert other_rng() == 21
 	assert rigged_rng() == 12
 	assert other_rng() == 22
+
+	# Test appending terms to the given list
+	l = [1]
+	rigged_rng = make_rigged_rng(l)
+	rigged_rng()
+	l.append(5)
+	assert rigged_rng() == 5
 
 test_rigged_rng()
