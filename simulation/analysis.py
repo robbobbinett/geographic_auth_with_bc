73140244b63d07010a9af4236e98c84a3443662a
robbobbinett <robinettryan8@gmail.com>
2019-12-10 08:06:34 -0600

    Making graphic analyses human-readable

--- simulation/analysis.py
+++ simulation/analysis.py
@@ -30,12 +30,17 @@ print("Doing adj_mat graphics...")
 onlyfiles = [f for f in listdir(cwd+"/pickle_dir/") if "adj_mat" in f]
 for f in tqdm(onlyfiles):
 	param_str = f.replace("_adj_mat.pkl", "")
+	param_list = param_str.split("_")
+	num_nodes = int(param_list[0])
+	cycle_rat = int(param_list[-1])
 	with open ("pickle_dir/"+f, "rb") as file:
 		adj_mats = load(file)
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 	connected_components_over_time(adj_mats, ax=ax)
-	ax.set_title(param_str)
+	ax.set_title("Connected Components over Time:\n"+str(num_nodes)+" Nodes,\n"+str(cycle_rat)+" Message Cycles per SPAN Cycle")
+	ax.set_xlabel("# SPAN Cycles")
+	ax.set_ylabel("# Connected Components")
 	fig.savefig("analysis/adj_mat/"+param_str+".pdf")
 	plt.close("all")
 
@@ -44,12 +49,17 @@ onlyfiles = [f for f in listdir(cwd+"/pickle_dir/") if "longest_chain_hist" in f
 print("Doing longest_chain_hist graphics...")
 for f in tqdm(onlyfiles):
 	param_str = f.replace("_longest_chain_hist.pkl", "")
+	param_list = param_str.split("_")
+	num_nodes = int(param_list[0])
+	cycle_rat = int(param_list[-1])
 	with open("pickle_dir/"+f, "rb") as file:
 		hists = load(file)
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 	heatmap_from_hists(hists, ax=ax)
-	ax.set_title(param_str)
+	ax.set_title("Longest Chains over Time:\n"+str(num_nodes)+" Nodes,\n"+str(cycle_rat)+" Message Cycles per Message-Passing Cycle")
+	ax.set_xlabel("# Message-Passing Cycles")
+	ax.set_ylabel("# Nodes Recognizing Chain")
 	fig.savefig("analysis/longest_chain_hist/"+param_str+".pdf")
 	plt.close("all")
 
